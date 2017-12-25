c2f8c0c2770a2c2fe6ca15c6e75be68352c12522
Austin Shin <shinaustin@gmail.com>
2017-12-24 19:08:32 -0800

    implemented model portion of db

--- server/models/index.js
+++ server/models/index.js
@@ -4,52 +4,54 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function () {
-    //fetch all messages 
-    //id, text, roomname, and username
-    var queryStr = 'SELECT messages.textID, messages.message, messages.createdAt, users.user, rooms.room /
-                      FROM messages / 
-                        LEFT OUTER JOIN users / 
-                          ON (users.usersID = messages.usersID) /
-                        LEFT OUTER JOIN rooms /
-                          ON (rooms.roomsID = messages.roomsID) /
-                      ORDER BY messages.textID desc'; 
+      //fetch all messages 
+      //id, text, roomname, and username
+      var queryStr = 'SELECT messages.textID, messages.message, messages.createdAt, users.user, rooms.room \
+                        FROM messages \
+                          LEFT OUTER JOIN users \ 
+                            ON (users.usersID = messages.usersID) \
+                          LEFT OUTER JOIN rooms \
+                            ON (rooms.roomsID = messages.roomsID) \
+                        ORDER BY messages.textID desc'; 
       //ORDER BY messages.createdAt desc';  
       //desc = descending (aka the newest one is at the top). asc = ascending (aka the oldest is at the top)
-    db.query(queryStr, function(err, results) {
-      callback(err, results); 
-    });
+      db.query(queryStr, function(err, results) {
+        callback(err, results); 
+      });
 
     }, // a function which produces all the messages
 
 
-    post: function () {
-    //create all messages
-    var queryStr = 'INSERT INTO messages(message, usersID';
-    db.query(queryStr, function(err, results) {
-      callback(err, results); 
-    });
+    post: function (params, callback) {
+      //create all messages
+      var queryStr = 'INSERT INTO messages(message, usersID, room) \
+                      VALUES (?, (SELECT usersID FROM user WHERE user = ? limit 1), ?)';
+      // you can apply a subquery to a query. the limit literally limits it to the first one we find.
+      db.query(queryStr, params, function(err, results) {
+        callback(err, results); 
+      });
     } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
     get: function (callback) {
-    //get all users   
-    var queryStr = 'SELECT * FROM users';
-    db.query(queryStr, function(err, results) {
-      callback(err, results); 
-    });
+      //get all users   
+      var queryStr = 'SELECT * FROM users';
+      db.query(queryStr, function(err, results) {
+        callback(err, results); 
+      });
 
 
     },
     post: function (paramas, callback) {
      
-    var queryStr = 'INSERT INTO users (user) VALUES (?)';
-    db.query(queryStr, params, function(err, results) {
-      callback(err, results); 
-    });
-    //create all users 
-    
+      var queryStr = 'INSERT INTO users (user) VALUES (?)';
+      db.query(queryStr, params, function(err, results) {
+        callback(err, results); 
+      });
+      //create all users 
+      
     }
   }
 };
